ed7a5d785bd7d5be7156f0d46f5ec62788489607
margishsojitra001 <margishsojitra4@gmail.com>
2026-07-29 14:09:51 +0530

    costomer class add to MiniBank

--- mini_project/MiniBank.java
+++ mini_project/MiniBank.java
@@ -12,6 +12,42 @@ enum MenuOption {
 
 public class MiniBank {
 
+    public static class Customer {
+        private String name;
+        private String email;
+        private String mobile;
+        private final String customerId;
+
+        private static long customerCounter = 101;
+
+        private static String generateCustomerId() {
+            return "CUST" + customerCounter++;
+        }
+
+        public Customer(String name, String email, String mobile) {
+            this.name = name;
+            this.email = email;
+            this.mobile = mobile;
+            this.customerId = generateCustomerId();
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public String getMobile() {
+            return mobile;
+        }
+
+        public String getCustomerId() {
+            return customerId;
+        }
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
