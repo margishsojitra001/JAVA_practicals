2bef53a466be418729e7116684f8a990556b3825
margishsojitra001 <margishsojitra4@gmail.com>
2026-08-04 23:38:37 +0530

    project update as per week 2 instruction

--- mini_project/MiniBank.java
+++ mini_project/MiniBank.java
@@ -48,6 +48,58 @@ public class MiniBank {
         }
     }
 
+    public static class Account {
+        private final String accountNumber;
+        private String ownerName;
+        private long balance;
+        private boolean active;
+
+        private static long accountCounter = 1;
+
+        private static String generateAccountNumber() {
+            return String.format("AC%04d", accountCounter++);
+        }
+
+        public Account(String ownerName, long balance) {
+            this.accountNumber = generateAccountNumber();
+            this.ownerName = ownerName;
+            this.balance = balance;
+            this.active = true;
+        }
+
+        public Account(String ownerName) {
+            this(ownerName, 0);
+        }
+
+        public void deposit(long amount) {
+            balance += amount;
+        }
+
+        public boolean withdraw(long amount) {
+            if (balance >= amount) {
+                balance -= amount;
+                return true;
+            }
+            return false;
+        }
+
+        public String getAccountNumber() {
+            return accountNumber;
+        }
+
+        public String getOwnerName() {
+            return ownerName;
+        }
+
+        public long getBalance() {
+            return balance;
+        }
+
+        public boolean isActive() {
+            return active;
+        }
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -57,11 +109,36 @@ public class MiniBank {
         System.out.println(bank.name());
         System.out.println(bank.branch());
 
+        Account[] accounts = new Account[3];
+
+        accounts[0] = new Account("Margish", 1000);
+        accounts[1] = new Account("Rahul", 500);
+        accounts[2] = new Account("Priya");
+
+        accounts[0].deposit(500);
+        accounts[0].withdraw(300);
+
+        accounts[1].deposit(1000);
+        accounts[1].withdraw(2000);
+
+        accounts[2].deposit(700);
+        accounts[2].withdraw(200);
+
+        System.out.println("\nAccount Details:");
+
+        for (Account account : accounts) {
+            System.out.println("Account Number : " + account.getAccountNumber());
+            System.out.println("Owner Name     : " + account.getOwnerName());
+            System.out.println("Balance        : ₹" + account.getBalance());
+            System.out.println("Active         : " + account.isActive());
+            System.out.println();
+        }
+
         boolean run = true;
 
         while (run) {
 
-            System.out.println("\n1. Open Account");
+            System.out.println("1. Open Account");
             System.out.println("2. Deposit");
             System.out.println("3. Withdraw");
             System.out.println("4. Transfer");
